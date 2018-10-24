e79e883df7caa1d3d7cf9bcff63936a1941a80dc
Paulo Gomes <pauloamgomes@gmail.com>
2018-10-24 09:37:56 +0100

    cockpit image paths fix

--- react-app/src/cockpit/utils.js
+++ react-app/src/cockpit/utils.js
@@ -4,8 +4,11 @@ export function getImageStyleUrl(style, image) {
   let path = "";
   if (image && image.styles !== undefined) {
     const img = image.styles.find(imgStyle => imgStyle.style === style);
+
     if (img && img.path) {
-      path = `/${Config.WEBSITE_BASE_PATH}/${img.path}`;
+      return `${Config.WEBSITE_BASE_PATH}/${img.path}`
+        .replace("//", "/")
+        .replace("//", "/");
     }
   }
   if (
